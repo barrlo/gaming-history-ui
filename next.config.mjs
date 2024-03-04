676ac83d26f303be6beaacbb5cdfecc49ad4672d
barrlo <dist.runner3@gmail.com>
2024-03-04 09:13:25 -0600

    set up linting test files and Jest lint rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    eslint: {
+        dirs: ['src', 'tests']
+    }
 };
 
 export default nextConfig;
