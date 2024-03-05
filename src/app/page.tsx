b522594369dceb7a838e41ddc06885cf1ac09269
barrlo <dist.runner3@gmail.com>
2024-03-04 21:26:49 -0600

    adding domain to the cache purge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-    return <h1>{'Barrlo Gaming History'}</h1>;
+    return (
+        <>
+            <h1>{'Barrlo Gaming History'}</h1>
+            <ul>
+                <li>{'World of Warcraft'}</li>
+                <li>{'Path of Exile'}</li>
+            </ul>
+        </>
+    );
 }
