ab8efbfe7ee284c243e00485058340ea95e14020
barrlo <dist.runner3@gmail.com>
2024-03-04 21:11:43 -0600

    add Material UI

---
+++ src/theme.ts
@@ -0,0 +1,24 @@
+'use client';
+import {Roboto} from 'next/font/google';
+import {createTheme} from '@mui/material/styles';
+
+const roboto = Roboto({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap'
+});
+
+// export const lightTheme = createTheme({
+//     typography: {
+//         fontFamily: roboto.style.fontFamily
+//     }
+// });
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: 'dark'
+    },
+    typography: {
+        fontFamily: roboto.style.fontFamily
+    }
+});
